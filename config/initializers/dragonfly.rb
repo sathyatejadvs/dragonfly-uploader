da6222b5b7e7802dad6df1be7b2923d066cf4cac
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-10-15 15:46:52 +0530

    Provided AWS s3 credentials in the dragonflt initializer file

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,16 +1,21 @@
 require 'dragonfly'
+require 'dragonfly/s3_data_store'
 
 # Configure
 Dragonfly.app.configure do
-  plugin :imagemagick
+	plugin :imagemagick
 
-  secret "b4821436115176d48f38b9a0e2003415ad79614653cffb82c8e9188887945999"
+	secret "b4821436115176d48f38b9a0e2003415ad79614653cffb82c8e9188887945999"
 
-  url_format "/media/:job/:name"
+	url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+	datastore :s3,
+	bucket_name: 'satya-ecommerce-app',
+	access_key_id: 'AKIAIZ6T2XDVFKZTQMQA',
+	secret_access_key: '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',
+	url_scheme: 'https',
+	region: 'ap-south-1'
+	
 end
 
 # Logger
@@ -21,6 +26,6 @@ Rails.application.middleware.use Dragonfly::Middleware
 
 # Add model functionality
 ActiveSupport.on_load(:active_record) do
-  extend Dragonfly::Model
-  extend Dragonfly::Model::Validations
+	extend Dragonfly::Model
+	extend Dragonfly::Model::Validations
 end
