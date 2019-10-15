5336d675247fcf14b4f03b7d42725cbe4679d2da
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-10-15 15:32:22 +0530

    Refactored the photos controller params

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,27 +1,25 @@
 class PhotosController < ApplicationController
-	def index
-		@photos = Photo.all
-	end
+  def index
+    @photos = Photo.all
+  end
 
-	def new
-		@photo = Photo.new
-	end
+  def new
+    @photo = Photo.new
+  end
 
-	def create
-		@photo = Photo.new(photo_params)
-		if @photo.save?
-			redirect_to photos_path
-		else
-			render :new
-		end
-	end
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      flash[:success] = "Photo saved!"
+      redirect_to photos_path
+    else
+      render 'new'
+    end
+  end
 
-	def destroy
-	end
+  private
 
-	private
-
-	def photo_params
-		parms.require(:photo).permit(:avatar, :title)
-	end
+  def photo_params
+    params.require(:photo).permit(:avatar, :title)
+  end
 end
