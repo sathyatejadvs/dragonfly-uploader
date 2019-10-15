05ff49f71e1961875b5bc1915d00343bfc86a9e7
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-10-15 14:45:52 +0530

    Created index, new, create and destroy actions for photos controller
    - Provided routing to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,27 @@
 class PhotosController < ApplicationController
+	def index
+		@photos = Photo.all
+	end
+
+	def new
+		@photo = Photo.new
+	end
+
+	def create
+		@photo = Photo.new(photo_params)
+		if @photo.save?
+			redirect_to photos_path
+		else
+			render :new
+		end
+	end
+
+	def destroy
+	end
+
+	private
+
+	def photo_params
+		parms.require(:photo).permit(:avatar, :title)
+	end
 end
